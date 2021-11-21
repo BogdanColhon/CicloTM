7491137700fc83305e06c4599fe30f1e779ef6d2
BogdanColhon <nbat99@yahoo.com>
2021-11-21 21:15:32 +0200

    Created Navigation Bar

--- app/src/main/java/com/example/ciclotm/MapsActivity.java
+++ app/src/main/java/com/example/ciclotm/MapsActivity.java
@@ -1,20 +1,24 @@
 package com.example.ciclotm;
 
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
-import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MapsActivity extends AppCompatActivity {
-    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        button=findViewById(R.id.button);
-        ActionBar ab=getSupportActionBar();
-        ab.setTitle("Hărți");
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this,  R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+    }
+    public void setActionBarTitle(String title) {
+        getSupportActionBar().setTitle(title);
     }
 }
