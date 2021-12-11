06b5d079874a30d9b4aa480bd916dddf3eaa7c9a
BogdanColhon <nbat99@yahoo.com>
2021-12-11 17:35:56 +0200

    Created the StolenBikeLocationActivity | Get location by using the Current Location Button

--- app/src/main/java/com/example/ciclotm/MapsFragment.java
+++ app/src/main/java/com/example/ciclotm/MapsFragment.java
@@ -1,6 +1,7 @@
 package com.example.ciclotm;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 
@@ -13,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -23,6 +25,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -82,6 +85,24 @@ public class MapsFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_maps, container, false);
         mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        FloatingActionButton reportButton = view.findViewById(R.id.reportFloatingButton);
+        FloatingActionButton mapLayerButton = view.findViewById(R.id.layersFloatingButton);
+
+        reportButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(getActivity(), StolenBikeLocationActivity.class);
+                getActivity().startActivity(myIntent);
+            }
+        });
+
+        mapLayerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Stolen bike",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         try {
             MapsInitializer.initialize(getActivity().getApplicationContext());
         } catch (Exception e) {
@@ -176,4 +197,5 @@ public class MapsFragment extends Fragment {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
     }
+
 }
