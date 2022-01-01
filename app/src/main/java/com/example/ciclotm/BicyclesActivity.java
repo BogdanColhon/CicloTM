bcfce3b63dd2c0e78b5ebb94d3bf84f369f090f8
BogdanColhon <nbat99@yahoo.com>
2022-01-01 14:12:52 +0200

    BicyclesActivity toolbar functionality

--- app/src/main/java/com/example/ciclotm/BicyclesActivity.java
+++ app/src/main/java/com/example/ciclotm/BicyclesActivity.java
@@ -1,9 +1,13 @@
 package com.example.ciclotm;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -11,33 +15,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class BicyclesActivity extends AppCompatActivity {
-    private ImageView left_icon;
-    private ImageView right_icon;
-    private Toolbar toolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bicycles);
 
-        left_icon = findViewById(R.id.left_icon);
-        right_icon = findViewById(R.id.right_icon);
-        toolbar = findViewById(R.id.action_bar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitle("Bicilete");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Biciclete");
+        actionBar.setDisplayHomeAsUpEnabled(true);
 
-        left_icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BicyclesActivity.this, "Left", Toast.LENGTH_SHORT).show();
-            }
-        });
-        right_icon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(BicyclesActivity.this, "Right", Toast.LENGTH_SHORT).show();
-                System.out.println("apasat");
-            }
-        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.action_bar_bicycles,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId())
+        {
+            case R.id.add:
+                Toast.makeText(this,"Bicicletă adaugată!",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return super.onSupportNavigateUp();
+
     }
 }
